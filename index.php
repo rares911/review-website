29d32c721c9c8eb5be823d94035680d4d61d8343
Rares Cristea <104209897+rares911@users.noreply.github.com>
2022-09-28 17:40:53 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<title>Review produse</title>
+<link rel="stylesheet" href="home.css">
+
+<body class="reviewBody">
+    
+    <h1>Produse</h1>
+    <br>
+
+    <div class="prod">
+
+    <div class="baterii"> 
+    Baterii Alcaline AA
+    <br>
+    <img src="baterii.jpg">
+     <a href="baterii.php"><button>Adauga Recenzie</button></a>
+    </div>   
+
+    <br>
+    <div class="mouse"> 
+        Mouse gaming wireless
+        <br>
+        <img src="mouse.jpg">
+        <a href="mouse.php"><button>Adauga Recenzie</button></a>
+        </div>   
+    <br>
+    <div class="vin"> 
+        Vin Rose 0.75L
+        <br>
+        <img src="vin.jpg">
+        <a href="vin.php"><button>Adauga Recenzie</button></a>
+        </div>   
+    <br>
+    <div class="ssd"> 
+        SSD Extern 1TB
+        <br>
+        <img src="ssd.jpg">
+        <a href="ssd.php"><button>Adauga Recenzie</button></a>
+        </div>   
+
+    </div>
+
+    <br>
+    <a href="home.html"><button>Inapoi la pagina principala</button></a>
+</body>
+
+</html>
